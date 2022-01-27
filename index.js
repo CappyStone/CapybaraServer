536599ea0732aa151250bdd7e9e6d61c41ca6da3
Justin Proulx <jawproulx2001@icloud.com>
2022-01-26 20:02:00 -0500

    add firebase admin

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const { initializeApp } = require('firebase-admin/app');
+
 const app = express()
 const port = process.env.port || 3001
+
+const firebase = initializeApp();
+const firebaseAuth = getAuth();
 
 app.get('/test', (req, res) => {
     res.json({ test: "SUCCESSED" });
