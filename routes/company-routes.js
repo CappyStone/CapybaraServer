efcbf279958e67f02a81a2215681b3de4ae8c9a4
Justin Proulx <jawproulx2001@icloud.com>
2022-02-16 17:30:40 -0500

    Switch db API to POST, check returned items exist

--- routes/company-routes.js
+++ routes/company-routes.js
@@ -1,30 +1,40 @@
 const db = require('../access-db');
 
 module.exports = function (app) {
-    app.get('/getCompanyData', async (req, res) => {
+    app.post('/getCompanyData', async (req, res) => {
         //api URI is http://localhost:3001/getCompanyData?userEmail=ENTERUSEREMAILHERE
-        const userEmail = req.query.userEmail;
+        const userEmail = req.body.userEmail;
 
         //response type
         res.contentType('application/json');
 
         //change this to info from the db
-        var items = await db.getCompanyData(userEmail);
-
+        var items = Object.assign({}, await db.getCompanyData(userEmail)); // combine the result with an empty object to ensure items is not undefined
+        var size = Object.keys(items).length; // get the number of keys in the object
+        
         //send the response
-        res.json(items);
+        if (size > 0) {
+            res.json(items);
+        } else {
+            res.json({})
+        }
     })
 
-    app.get('/getEquipmentData', async (req, res) => {
+    app.post('/getEquipmentData', async (req, res) => {
         //api URI is http://localhost:3001/getEquipmentId?equipmentId=ENTERID
-        const equipmentId = req.query.equipmentId;
+        const equipmentId = req.body.equipmentId;
         //response type
         res.contentType('application/json');
 
         //change this to info from the db
-        var items = await db.getEquipmentData(equipmentId);
+        var items = Object.assign({}, await db.getEquipmentData(equipmentId)); // combine the result with an empty object to ensure items is not undefined
+        var size = Object.keys(items).length // get the number of keys in the object
 
         //send the response
-        res.json(items);
+        if (size > 0) {
+            res.json(items);
+        } else {
+            res.json({})
+        }
     })
 }
