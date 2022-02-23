b4c0096d8123ac74a38c23d1e5ffe3c8053c9d85
elisecloutier <elisecloutier@hotmail.com>
2022-02-23 15:01:57 -0500

    isEmployeeAdmin API to check if an employee is admin

--- routes/company-routes.js
+++ routes/company-routes.js
@@ -119,4 +119,24 @@ module.exports = function (app) {
             res.json({})
         }
     })
+
+    app.post('/isEmployeeAdmin', async (req, res) => {
+        const userEmail = req.body.userEmail;
+
+        //response type
+        res.contentType('application/json');
+
+        //change this to info from the db
+        var items = Object.assign({}, await db.isEmployeeAdmin(userEmail)); // combine the result with an empty object to ensure items is not undefined
+        var size = Object.keys(items).length; // get the number of keys in the object
+        
+        //send the response
+        if (size > 0) {
+            res.json(items);
+        } else {
+            res.json({})
+        }
+
+       
+    })
 }
