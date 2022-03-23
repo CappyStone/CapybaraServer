bf06547622bc83b1a805a958b3a5e757153bd2c2
Gregory Mitchell <43375116+GregMitch@users.noreply.github.com>
2022-03-23 14:01:14 -0400

    Update apiTest.js

--- testing/apiTest.js
+++ testing/apiTest.js
@@ -5,7 +5,7 @@ const assert = require('assert');
 const app = require('../index.js');
 const should = chai.should();
 
-const config = require("../config");
+// const config = require("../config");
 // const endpoint = config.endpoint;
 // const key = config.key;
 const endpoint = process.env.CUSTOMCONNSTR_CosmosAddress;
